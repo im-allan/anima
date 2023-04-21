e528adfe9329587c8a252e2b530f58a2023a4e22
Allan <al.silvaquiroz@gmail.com>
2023-04-20 23:48:39 -0400

    feat(main.jsx): added main file that renders the app

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
+import { SpaApp } from "./SpaApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <SpaApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);

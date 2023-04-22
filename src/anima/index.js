00ac947ecea9de033292305f9ad3cd893bcbf126
Allan <al.silvaquiroz@gmail.com>
2023-04-22 18:31:22 -0400

    feat(index.js): added barrel file for anima

---
+++ src/anima/index.js
@@ -0,0 +1,7 @@
+export * from "./components";
+export * from "./context";
+export * from "./data";
+export * from "./helpers";
+export * from "./hooks";
+export * from "./pages";
+export * from "./routes/AnimaRoutes";

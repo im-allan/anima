205f174bd8947da54df4f9e8d239fccb6551bfce
Allan <al.silvaquiroz@gmail.com>
2023-04-21 23:09:21 -0400

    feat(index.js): barrel file for components updated

---
+++ src/anima/components/index.js
@@ -0,0 +1,7 @@
+export * from "./carrousel";
+export * from "./footer/Footer";
+export * from "./main/main";
+export * from "./modal/Modal";
+export * from "./profile/Profile";
+export * from "./top/AnimaTopCarrousel";
+export * from "./intro/Intro";

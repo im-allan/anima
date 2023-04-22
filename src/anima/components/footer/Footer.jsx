c47739e12bbf3ea6c9cfae4b56080c1044d379a7
Allan <al.silvaquiroz@gmail.com>
2023-04-21 23:04:50 -0400

    feat(footer.jsx): added footer component, it is literally the footer of the page

---
+++ src/anima/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <footer className="pt-5 mt-5 mb-5">
+      <p className="pt-5 mt-5 text-center text-white align-text-bottom">
+        Made With &#10084; by{" "}
+        <a
+          className="text-white"
+          href="https://github.com/im-allan"
+          target="_blank"
+        >
+          @im-allan
+        </a>
+      </p>
+    </footer>
+  );
+};
